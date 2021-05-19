41ac7ca85c3e3cef44785c012274ce317516f980
57900 <57900@newington.nsw.edu.au>
2021-05-19 12:47:18 +1000

    Fix Up JS and pages

---
+++ HTML/JS/scoreBoardFive.js
@@ -0,0 +1,7 @@
+function fiveSet(){
+    console.log("hello")
+    var homeTeam = document.getElementById("home").innerHTML
+    var homeTeamReplace = document.getElementById("homeTeam").innerHTML
+    homeTeamReplace = homeTeam
+    location.replace("http://127.0.0.1:5500/HTML/ScoreBoard5.HTML")
+}
