0fec7f99ad8ad7b5b423e3d88c3b51d9c5b4e8b0
57900 <57900@newington.nsw.edu.au>
2021-06-03 15:20:18 +1000

    Worked on making keys change the score - part 1

--- HTML/JS/scoreBoardFive.js
+++ HTML/JS/scoreBoardFive.js
@@ -44,8 +44,69 @@ function enterFiveSet(){
     var awayPlayerSix = document.getElementById("homePlayerSix").value
     var awayLibero = document.getElementById("homePlayerLib").value
 
-    document.getElementById("namesOfTeams").innerHTML = homeTeamName + " VS " + awayTeamName
+    if (homeTeamName == ""){
+        if (awayTeamName == ""){
+            document.getElementById("namesOfTeams").innerHTML = "Home" + " VS " + "Away"
+        }else{
+            document.getElementById("namesOfTeams").innerHTML = "Home" + " VS " + awayTeamName
+        }
+    }else if (awayTeamName == ""){
+        if (homeTeamName == ""){
+            document.getElementById("namesOfTeams").innerHTML = "Home" + " VS " + "Away"
+        }else{
+            document.getElementById("namesOfTeams").innerHTML = homeTeamName + " VS " + "Away"  
+        }
+    }else{
+        document.getElementById("namesOfTeams").innerHTML = homeTeamName + " VS " + awayTeamName
+    }
+    
 
 }
 
 var homeScore = 0
+var awayScore = 0
+var awaySetsWon = 0
+var homeSetsWon = 0
+window.addEventListener("keyup", onKeyUp, false);
+
+
+function onKeyUp(event){
+    var keyCode = event.keyCode;
+    switch(keyCode){
+     case 84:  //t key Home Sets goes up
+          homeSetsWon = homeSetsWon + 1
+          document.getElementById("setsWon").innerHTML = homeSetsWon + " Sets " + awaySetsWon
+      break;
+      case 71:  //g key Home Sets goes down
+          homeSetsWon = homeSetsWon - 1
+          document.getElementById("setsWon").innerHTML = homeSetsWon + " Sets " + awaySetsWon
+      break;
+      case 87:  //W Key Home score goes up
+          homeScore = homeScore + 1
+          if (homeSetsWon + awaySetsWon == 0){
+              document.getElementById("firstSetOn").innerHTML = homeScore + " VS " + awayScore
+              document.getElementById("firstSet").innerHTML = homeScore + " VS " + awayScore
+          }
+      break;
+      case 83:  //s Key Home score goes down
+          homeScore = homeScore - 1
+          document.getElementById("firstSetOn").innerHTML = homeScore + " VS " + awayScore
+      break;
+      case 89:  //y key Away Sets goes up
+          awaySetsWon = awaySetsWon + 1
+          document.getElementById("setsWon").innerHTML = homeSetsWon + " Sets " + awaySetsWon
+      break;
+      case 72:  //h key Away sets goes down
+          awaySetsWon = awaySetsWon - 1
+          document.getElementById("setsWon").innerHTML = homeSetsWon + " Sets " + awaySetsWon
+      break;
+      case 73:  //i key Away score goes up
+          awayScore = awayScore + 1
+          document.getElementById("firstSetOn").innerHTML = homeScore + " VS " + awayScore
+      break;
+      case 75:  //k key Away score goes down
+          awayScore = awayScore - 1
+          document.getElementById("firstSetOn").innerHTML = homeScore + " VS " + awayScore
+      break;
+    }
+  }
